db66fcb5bbdc754ea3459d6eb0e2b1eb098557f9
Alexander F. Rødseth <rodseth@gmail.com>
2023-08-18 21:04:06 +0200

    Add checks and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use std::io;
 use std::fmt;
+use std::io;
 
 // Error for copying machine code to executable memory
 #[derive(Debug, Clone)]
@@ -15,13 +15,19 @@ impl std::error::Error for MemCopyError {}
 
 // Convert a space-separated hex string to a Vec<u8>
 pub fn string_to_bytes(hexstring: &str) -> Result<Vec<u8>, std::num::ParseIntError> {
-    hexstring.split_whitespace()
+    hexstring
+        .split_whitespace()
         .map(|s| u8::from_str_radix(s, 16))
         .collect()
 }
 
 // Execute machine code and return the value
 pub fn execute(code: &[u8]) -> Result<i32, Box<dyn std::error::Error>> {
+    // Check for empty code
+    if code.is_empty() {
+        return Err(Box::new(MemCopyError));
+    }
+
     let size = code.len();
     let executable_area = unsafe {
         libc::mmap(
@@ -40,7 +46,45 @@ pub fn execute(code: &[u8]) -> Result<i32, Box<dyn std::error::Error>> {
 
     unsafe {
         std::ptr::copy_nonoverlapping(code.as_ptr(), executable_area, size);
+
         let f: extern "C" fn() -> i32 = std::mem::transmute(executable_area);
-        Ok(f())
+        let result = std::panic::catch_unwind(|| f());
+
+        libc::munmap(executable_area as *mut libc::c_void, size);
+
+        match result {
+            Ok(val) => Ok(val),
+            Err(_) => Err(Box::new(MemCopyError)),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_string_to_bytes() {
+        let input = "00 ff a2 5e";
+        let output = string_to_bytes(input);
+        match output {
+            Ok(v) => assert_eq!(v, vec![0x00, 0xff, 0xa2, 0x5e]),
+            Err(_) => panic!("Failed to convert hex string to bytes"),
+        }
+    }
+
+    #[test]
+    fn test_string_to_bytes_error() {
+        let input = "00 ffzz a2 5e";
+        let output = string_to_bytes(input);
+        assert!(output.is_err());
+    }
+
+    // Test the error case instead of executing machine code in the test
+    #[test]
+    fn test_execute_error() {
+        let code = vec![];
+        let result = execute(&code);
+        assert!(result.is_err());
     }
 }
